6b85dfd123cd217b6fa3887cd9c224ebf9d41fef
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-03-11 21:54:55 +0400

    Added test cases for commentsReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import CommentReducer from './comment_reducer';
+import CommentReducer from './comments';
 
 const rootReducer = combineReducers({
   comments: CommentReducer
